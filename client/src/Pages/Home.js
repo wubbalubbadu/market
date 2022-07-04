06531cd3751199e01dc0db9c0aaffa83c7979421
Juliaaaachu <juliachu910125@gmail.com>
2022-07-04 16:39:28 +0800

    materialuis dependencies installed

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import { Typography} from "@material-ui/core";
 
 
 function Home() {
   return (
     <div>
-        <h1>THIS IS HOME PAGE </h1>
+        <Typography variant='h1'> THIS IS HOEM PAGE </Typography>
         <Link to="/post_items">Click to post item</Link>
         <Link to="/request_item">Click to request items</Link>
     </div>
