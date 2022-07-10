57bccb5ef9611113931cf48f49d31d44523d513f
Juliaaaachu <juliachu910125@gmail.com>
2022-07-10 12:40:49 +0800

    add sidebar post navbar

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -4,12 +4,17 @@ import Add from '@mui/icons-material/Add';
 import {Typography,Button} from "@material-ui/core";
 import {ActionButton} from "../themes/ActionButton.js"
 import { ThemeProvider } from '@mui/material';
-
+import Sidebar from '../Components/Sidebar.jsx';
+import Post from '../Components/Post.jsx';
+import Navbar from '../Components/Navbar.jsx';
 
 function Home() {
   return (
     <div>
       <ThemeProvider>
+        <Sidebar/>
+        <Post/>
+        <Navbar/>
         <Typography variant='h2'> THIS IS HOME PAGE </Typography>
 
         <Button 
@@ -26,14 +31,15 @@ function Home() {
           component={Link}
           to="/request_item"
           variant="contained" 
-          color="primary"
+          color = "otherColor"
         >
           REQUEST ITEMS
         </Button>
         
         <ActionButton> Home </ActionButton>
+
       </ThemeProvider>
-        
+
     </div>
   );
 }
