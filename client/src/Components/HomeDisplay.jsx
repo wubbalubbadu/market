a1f490a083b6fbbc2a22d5872cf01de408c236e7
wubbalubbadu <wavgoddbye@gmail.com>
2022-08-17 00:19:30 -0700

    deployment

--- client/src/Components/HomeDisplay.jsx
+++ client/src/Components/HomeDisplay.jsx
@@ -8,6 +8,7 @@ import { Box, Stack } from "@mui/material";
 
 function HomeDisplay() {
   const products = useSelector((state) => state.productsReducer.products);
+  console.log('products at homedisplay', products)
   const dispatch = useDispatch();
   const fetchProducts = async () => {
     const response = await axios
@@ -16,7 +17,7 @@ function HomeDisplay() {
         console.log(err);
       });
     dispatch(setProducts(response.data.products));
-    
+
     // dispatch({
     //   type: 'SET_PRODUCTS', // type is required
     //   payload: response.data.products
